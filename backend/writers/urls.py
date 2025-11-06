96850e335afb17e0c905f900964f39930c9c2836
shafayet-N <neutronos.u@gmail.com>
2025-11-06 18:54:18 +0600

    Update urls.py

--- backend/writers/urls.py
+++ backend/writers/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'writers'
+
 urlpatterns = [
-    path("profile/edit/", views.edit_profile, name="profile_edit"),
     path("profile/", views.view_profile, name="profile"),
+    path("profile/edit/", views.edit_profile, name="profile_edit"),
 ]
