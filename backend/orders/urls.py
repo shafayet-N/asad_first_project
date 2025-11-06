a9fd5d0db92f3eed93ee11d5e65138549e856b22
shafayet-N <neutronos.u@gmail.com>
2025-11-06 18:53:32 +0600

    Update urls.py

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'orders'
+
 urlpatterns = [
     path("new/", views.new_order, name="new"),
     path("dashboard/", views.dashboard, name="dashboard"),
